15eb4b34edb3b43ea156e865c7c0a33fefb12b4c
chiragdoctor <chirag.doctor@gmail.com>
2016-08-30 03:28:47 +0100

    add data support as json file

---
+++ app/data/items.js
@@ -0,0 +1,8 @@
+var items = [
+		{name: "Peas", image_url:"../public/img/peas.jpg", price: 0.95}, 
+		{name: "Eggs", image_url:"../public/img/egg.jpg", price: 2.10},
+		{name: "Milk", image_url:"../public/img/milk.jpg", price: 1.30}, 
+		{name: "Beans", image_url:"../public/img/beans.jpg", price: 0.73} 
+];
+
+module.exports = items;
